1fee71145e20c0696972fe3d8bb4b4fb0be57b64
AnkitaSabale99 <93145678+AnkitaSabale99@users.noreply.github.com>
2022-04-26 13:41:59 +0530

    Add files via upload

---
+++ Stringclass.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+public class Stringclass 
+{
+    public static void main(String[] args)
+    {
+        String str = "Ankita";
+        String str2 = new String("Ankita");
+
+        if(str==str2)
+        {
+            System.out.println("matched");
+        }
+        else
+
+        if(str==str2)
+        {
+            System.out.println("matched");
+        }
+        else
+        {
+            System.out.println("Not matched");
+        }
+    
+
+
+    }
+}
